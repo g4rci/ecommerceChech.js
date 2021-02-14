58dabdfb2d5cc81db3bea0fdc1c3cc2b0787fd4c
M.A.Garcia <m.angel.gf04.com>
2021-02-14 19:40:05 +0100

    ecommerce with Chec.js tutorial first 2 hours

---
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -0,0 +1,90 @@
+import React, {useState, useEffect } from 'react'
+import { InputLabel, Select, MenuItem, Button, Grid, Typography, Menu } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import { commerce, Commerce } from '../../lib/commerce';
+
+import FormInput from './CustomTextField';
+
+const AddressForm = ( { checkoutToken }) => {
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingSubDivisions, setShippingSubDivisions] = useState([]);
+    const [shippingSubDivision, setShippingSubDivision] = useState('');
+    const [shippingSubOptions, setShippingSubOptions] = useState([]);
+    const [shippingSubOption, setShippingSubOption] = useState('');
+    const methods = useForm();
+
+    const countries = Object.entries(shippingCountries).map(([code, name]) => ({ id: code, label: name}))
+    const subdivisions = Object.entries(shippingSubDivisions).map(([code, name]) => ({ id: code, label: name}))
+    
+    const fetchShippingCountries = async (checkoutTokenId) => {
+        const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
+
+        setShippingCountries(countries);
+        setShippingCountry(Object.keys(countries)[0]);
+    }
+
+    const fetchSubDivisions = async (countryCode) => {
+        const { subdivisions } = await commerce.services.localeListSubdivisions(countryCode);
+
+        setShippingSubDivisions(subdivisions);
+        setShippingSubDivision(Object.keys(subdivisions)[0])
+    }
+
+    useEffect(() => {
+        fetchShippingCountries(checkoutToken.id);
+      }, []);
+
+    useEffect(() => {
+        if(shippingCountry) fetchSubDivisions(shippingCountry)
+    },[shippingCountry]);
+
+    return (
+        <>
+            <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit=''>
+                    <Grid container spacing={3}>
+                        <FormInput required name='firstName' label='First name' />
+                        <FormInput required name='lastName' label='Last name' />
+                        <FormInput required name='address1' label='Address' />
+                        <FormInput required name='email' label='Email' />
+                        <FormInput required name='city' label='City' />
+                        <FormInput required name='zip' label='ZIP / Postalcode' />
+                        <Grid item xd={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+                            <Select value={shippingCountry} fullWidth onChange={(e) => setShippingCountry(e.target.value)}>
+                                {countries.map((country) => (
+                                    <MenuItem key={country.id} value={country.id}>
+                                        {country.label}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </Grid>
+                        <Grid item xd={12} sm={6}>
+                            <InputLabel>Shipping Subdivision</InputLabel>
+                            <Select value={shippingSubDivision} fullWidth onChange={(e) => setShippingSubDivision(e.target.value)}>
+                                {subdivisions.map((subdivision) => (
+                                    <MenuItem key={subdivision.id} value={subdivision.id}>
+                                        {subdivision.label}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </Grid>
+                        {/* <Grid item xd={12} sm={6}>
+                            <ImputLabel>Shipping Options</ImputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem key={} value={}>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+                        </Grid> */}
+                    </Grid>
+                </form>
+            </FormProvider>
+        </>
+    )
+}
+
+export default AddressForm
